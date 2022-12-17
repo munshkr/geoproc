e211078dfe35467333b060f07579613381564725
Damián Silvani <munshkr@gmail.com>
2022-12-17 11:17:12 -0300

    docs: Install sphinx_rtd_theme and configure it

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "Damián Silvani"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_nb"]
+extensions = ["myst_nb", "sphinx_rtd_theme"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
@@ -21,5 +21,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
